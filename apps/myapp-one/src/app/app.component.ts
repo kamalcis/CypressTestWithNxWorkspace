9439097aa7e119ebabeaaec01627745b9c31ec6f
Kamal <abu.kamal@marel.com>
2023-09-30 14:49:30 +0600

    Initial commit

---
+++ apps/myapp-one/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myorg-apps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'myapp-one';
+}
